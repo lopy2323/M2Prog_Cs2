2b09d6858437948a45d30ecdf10c5cb4c6d2ff89
lopy2323 <r.keeren@hotmail.com>
2025-01-27 11:45:56 +0100

    hallo

---
+++ 04.list/04.list/Formulier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.list
+{
+    internal class Formulier
+    {
+        internal int Sterren { get; set; }
+
+        internal String Feedback { get; set; }
+
+    }
+}
---
+++ 04.list/04.list/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _04.list
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();
+        }
+        void Run()
+        {
+            double[] prijzen = new double[] { 0.99, 6.60, 10.11,99.99 };
+            string[] artiekelen = new string[] { "snoepje", "luxe broodje", "luch menu","oplader" };
+            Formulier[] formulieren = new Formulier[2];
+            formulieren[0] = new Formulier()
+            {
+                Feedback = "prijzig maar lekker",
+                Sterren = 5
+            };
+            formulieren[1] = new Formulier()
+            {
+                Feedback = "niet lekker",
+                Sterren = 2
+            };
+            for (int i = 0; i < formulieren.Length; i++)
+            {
+                Console.WriteLine(formulieren[i]);
+            }
+            foreach (Formulier formulier in formulieren)
+            {
+                
+            }
+
+        }
+    }
+}
